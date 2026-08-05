64f5b637080af497f3effda82d4906bf295c8d56
maickmoreno12 <maickcupper@gmail.com>
2026-08-05 04:53:05 -0500

    implement header

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className=" min-h-screen text-gray-400">
+      {/* Header */}
+      <Header />
+      <div className="container py-10">{children}</div>
+    </main>
+  );
+};
+
+export default layout;
---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const NAV_ITEM = [
+  { href: "/", label: "Dashboard" },
+  { href: "/search", label: "Search" },
+  { href: "/watchlist", label: "Watchlist" },
+];
